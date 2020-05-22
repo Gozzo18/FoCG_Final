9988b22cc0ba8394997d7008796518b58e6f6d39
riccardo <go_riky@hotmail.it>
2020-05-22 09:58:07 +0200

    Works but without gaussianization. Some ghosting effects present.

---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+./bin/yscenetrace tests/01_brick/brick1.json -o out/lowres/01_brick_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/02_brick/brick2.json -o out/lowres/02_brick_720_256.jpg -t path -s 256 -r 720
+./bin/yscenetrace tests/03_c10/c10.json -o out/lowres/03_c10_720_256.jpg -t path -s 256 -r 720
